cecde839832aa2cb98d5bf612cebf5fc4f5d2aef
Florencia <flor.anabella@gmail.com>
2021-10-23 15:51:32 -0300

    entrega clase 1 y 2

---
+++ clase1Fleitas/coder.js
@@ -0,0 +1,5 @@
+"strict mode"
+
+let nombreIngresado = prompt("Ingresá tu nombre");
+let apellidoIngresado = prompt("Ingresá tu apellido");
+alert("Hola" + " " + nombreIngresado + " " + apellidoIngresado);
---
+++ clase2Fleitas/coder.js
@@ -0,0 +1,10 @@
+"strict mode"
+
+let ingreseNombre = prompt("Ingresa tu nombre");
+let ingreseEdad = parseInt(prompt("Ingrese edad"));
+
+if (ingreseEdad < 18) {
+    alert("You shall not pass")
+} else {
+    alert("Bienvenido")
+}

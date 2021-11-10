4475ee2e967c7654ecc26cd2f0e08fa4eddb0642
Florencia <flor.anabella@gmail.com>
2021-11-10 20:07:08 -0300

    Elementos basee del simulador

--- Simulador interactivo/script.js
+++ Simulador interactivo/script.js
@@ -1,70 +1,31 @@
-function elegirServicio() {
-    //Variables//
+'use strict';
 
-    window.alert("Precios: Carta natal = 3500");
-    window.alert("Precios: Lectura de Tarot = 500");
-    window.alert("Pidiendo más de un servicio te llevás descuentos!");
-    let nombre = prompt("Ingresar email");
-    let cartaNatal = prompt("Deseás comprar una carta natal? s/n");
-    let lecturaDeTarot = prompt("Deseás una lectura de tarot? s/n");
-    let cantidadDePreguntas = parseInt(prompt("Cuántas preguntas querés? Ingresar número"));
-
-    //Condicionales//
-    if (cartaNatal == "s") {
-        cartaNatal = true;
-    } else {
-        cartaNatal = false
-    }
-    if (lecturaDeTarot == "s") {
-        lecturaDeTarot = true;
-    } else {
-        lecturaDeTarot = false
-    }
-
-    //Llamado a la función//
-    calcularDescuento(cartaNatal, lecturaDeTarot, cantidadDePreguntas);
-}
-
-//función 2//
-
-function calcularDescuento(cartaNatal, lecturaDeTarot, cantidadDePreguntas) {
-
-    let precioCarta = 3500;
-    let precioTarot = 500
-
-    if (cartaNatal == true) {
-        if (lecturaDeTarot == true) {
-            if (cantidadDePreguntas > 3) {
-                mostrarResultado((precioCarta + precioTarot) * 0.8);
-            } else {
-                mostrarResultado((precioCarta + precioTarot) * 0.9);
-            }
-        } else {
-            mostrarResultado(precioCarta);
-        }
-
-    } else {
-        if (lecturaDeTarot == true) {
-            if (cantidadDePreguntas > 3) {
-                mostrarResultado(precioTarot * 0.8);
-            } else {
-                mostrarResultado(precioTarot);
-            }
-        } else {
-            window.alert("Gracias! hasta la próxima!");
-        }
+class Madera {
+    constructor(nombre, color, descripcion) {
+        this.color = color;
+        this.descripcion = descripcion;
+        this.nombre = nombre;
     }
 }
 
-function mostrarResultado(costoTotal) {
-    window.alert("Tu precio final es " + costoTotal);
+class Nucleo {
+    constructor(nombre, propiedad) {
+        this.nombre = nombre;
+        this.propiedad = propiedad;
+    }
 }
 
-
-elegirServicio();
-
-/*si pide carta natal + lectura de tarot = descuento, 
-si pide más de 3 preguntas = descuento
-si no pide carta natal + lectura de tarot = no descuento
-si no pide mas de tres preguntas = no descuento
-*/
+let maderas = [
+    new Madera("Manzano", "Gris plata", "Madera noble que se lleva mal con las artes obscuras"),
+    new Madera("Cerezo", "Caoba", "Una madera exótica que favorece a gente extravagante"),
+    new Madera("Sauce", "Peltre", "Una madera poco común ideal para magia curativa. Muy atractiva. Rechaza vanidosos"),
+    new Madera("Serbal", "Obscura", "Madera sólida y confiable, favorece los hechizos de protección"),
+    new Madera("Laurel", "Castaño", "Se dice que esta madera es incapaz de cometer actos deshonrosos, pero sin duda favorece a los que buscan gloria")
+];
+let nucleos = [
+    new Nucleo("Pelo de Veela", "Las varitas con este núcleo son temperamentales. Manejar con cuidado."),
+    new Nucleo("Pluma Fenix", "Las varitas con este núcleo son generalistas. No es habitual ser elegido por una de estas."),
+    new Nucleo("Pelo de Unicornio", "Las varitas con este núcleo se vinculan fuertemente con su primer dueño. Son consistentes y confiables"),
+    new Nucleo("Pluma de cola de un Pájaro de Trueno", "Las varitas con este núcleo son muy poderosas, pero dominarlas es realmente difícil. Muy valoradas por transmutadores."),
+    new Nucleo("Pelo de Kelpie", "Las varitas con este núcleo son fáciles de obtener. En el pasado se usaban mucho, pero ya no son tan frecuentes.")
+];
